fd4ec096f79ed2b0f7c3e0cd234e5d388585a644
Sharvendra <sharvendra2003@gmail.com>
2024-04-17 15:24:01 +0530

    add user and video model

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,4 +1,4 @@
-class APiError extends Error{
+class ApiError extends Error{
     constructor(
         statusCode,
         message="Something went wrong",
@@ -22,4 +22,4 @@ stack=""
 
 }
 
-export {APiError}
+export {ApiError}
