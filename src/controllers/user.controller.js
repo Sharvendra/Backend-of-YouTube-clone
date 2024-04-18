6328b2de3eda51c0491be2bc10c88c703d08e974
Sharvendra <sharvendra2003@gmail.com>
2024-04-18 10:52:00 +0530

    login and logout controllers are added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,25 @@ import { ApiError } from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating refresh and access token"
+    );
+  }
+};
+
 const registerUser = asynchandler(async (req, res) => {
   // res.status(200).json({
   //     message:"ok done"
@@ -40,14 +59,17 @@ const registerUser = asynchandler(async (req, res) => {
   console.log(req.files);
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-//   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  //   const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-let coverImageLocalPath;
-if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0)
-
-  if (!avatarLocalPath) {
-    throw new ApiError(400, "Avatar file is required");
-  }
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  )
+    if (!avatarLocalPath) {
+      throw new ApiError(400, "Avatar file is required");
+    }
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
@@ -74,8 +96,90 @@ if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.leng
 
   return res
     .status(201)
-    .json(new ApiResponse(200, createdUser, "User registered successfully"))
-    //  .end()
+    .json(new ApiResponse(200, createdUser, "User registered successfully"));
+  //  .end()
 });
 
-export { registerUser };
+const loginUser = asynchandler(async (req, res) => {
+  //req body -> data
+  // username or email
+  // find the user
+  // password check
+  // access and refresh token
+  //send cookie
+
+  const { email, username, password } = req.body;
+
+  if (!username || !email) {
+    throw new ApiError(400, "username or email is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(400, "User does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(200, {
+        user: loggedInUser,
+        accessToken,
+        refreshToken,
+      },
+      "User logged in Successfully"
+    )
+    );
+
+});
+
+const logoutUser = asynchandler(async(req,res)=>{
+ await User.findByIdAndUpdate(
+  req.user._id,
+  {
+    $set: {
+      refreshToken : undefined
+    }
+  },
+  {
+    new: true
+  }
+ )
+
+ const options = {
+  httpOnly: true,
+  secure: true,
+};
+
+return res
+.status(200)
+.clearCookie("accessToken", options)
+.clearCookie("refreshToken", options)
+.json(new ApiResponse(200, {}, "User logged Out"))
+})
+
+export { registerUser, loginUser, logoutUser };
